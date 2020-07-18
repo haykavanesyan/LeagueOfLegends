b5679f5bead34e82b3d1b1a0b954c0adb06a2ce5
haykavanesyan <haykavanesyan09@gmail.com>
2020-07-19 02:37:49 +0400

    Updates

---
+++ precache-manifest.ce15e522bfebf3f6cfa28f795d2bfb2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eab1f2235f58dd1cc7696cdd5687215",
+    "url": "/LeagueOfLegends/index.html"
+  },
+  {
+    "revision": "b784e62911e1b8674aed",
+    "url": "/LeagueOfLegends/static/css/main.4e16de2d.chunk.css"
+  },
+  {
+    "revision": "af586e785b9446039817",
+    "url": "/LeagueOfLegends/static/js/2.ebaddaf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LeagueOfLegends/static/js/2.ebaddaf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b784e62911e1b8674aed",
+    "url": "/LeagueOfLegends/static/js/main.e176b553.chunk.js"
+  },
+  {
+    "revision": "990516a3b959cb096a2f",
+    "url": "/LeagueOfLegends/static/js/runtime-main.1d289525.js"
+  },
+  {
+    "revision": "0d4c04f95f8f48f56531c2606999bdcc",
+    "url": "/LeagueOfLegends/static/media/deleted.0d4c04f9.png"
+  },
+  {
+    "revision": "729061e30dbe32a0b1c2382dc5ac97ce",
+    "url": "/LeagueOfLegends/static/media/remove.729061e3.png"
+  }
+]);
